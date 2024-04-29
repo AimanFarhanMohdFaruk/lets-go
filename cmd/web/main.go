4b58cf3250adfaddcea90228dc3391ac57c3d3e5
Aiman Farhan <aiman.faruk@surialabs.com>
2024-04-29 16:44:29 +0800

    feat: handlers folder

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,28 +1,38 @@
 package main
 
 import (
-	"flag"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+
+	"github.com/aiman-farhan/snippetbox/handlers"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	addr := flag.String("addr", ":3000", "HTTP Network Address")
-	flag.Parse()
+	addr := os.Getenv("PORT")
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	
 	mux := http.NewServeMux()
-
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", showHomePage) // $ symbol restructs this route to exact match on / only
+	mux.HandleFunc("GET /{$}", handlers.ShowHomePage) // $ symbol restructs this route to exact match on / only
 	
-	mux.HandleFunc("GET /snippet/view/{id}", showSnippet)
-	mux.HandleFunc("GET /snippet/create", newSnippetForm)
-	mux.HandleFunc("POST /snippet/create", createSnippet)
+	mux.HandleFunc("GET /snippet/view/{id}", handlers.ShowSnippet)
+	mux.HandleFunc("GET /snippet/create", handlers.NewSnippetForm)
+	mux.HandleFunc("POST /snippet/create", handlers.CreateSnippet)
 
-	log.Printf("Starting server on port %s", *addr)
+	logger.Info("Starting on server", "addr" , addr)
 
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	start := http.ListenAndServe(addr, mux)
+	logger.Error(start.Error())
+	os.Exit(1)
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/aiman-farhan/snippetbox
 go 1.22.1
 
 require github.com/a-h/templ v0.2.663
+
+require github.com/joho/godotenv v1.5.1
